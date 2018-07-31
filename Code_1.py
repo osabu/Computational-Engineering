991e6792cbd568b163fd5a9871b9effb0bab8229
osabu <osman.abuobaida1996@gmail.com>
2018-07-31 14:26:58 +0200

    Update Code_1.py

--- Code_1.py
+++ Code_1.py
@@ -6,72 +6,42 @@ Created on Sat Jul 28 15:53:05 2018
 Last update : 30.07.18,21:37
 """
 
-from dolfin import *
-xlength=1200.0
-ylength=700.0
-zlength=700.0
-
-mesh = BoxMesh (Point(0,0,0), Point(xlength,ylength,zlength),1200,700,700)
-V = VectorFunctionSpace(mesh, 'P', 1)
-cells = MeshFunction('size_t', mesh, 3, 0)
-facets = MeshFunction('size_t', mesh, 2, 0)
-dA = Measure('ds', domain=mesh, subdomain_data=facets, metdata={'quadreature_degree': 2})
-dV = Measure('dx', domain=mesh, subdomain_data=cells, metadata={'quadrature_degree': 2})
-left = CompiledSubDomain('near(x[0],0) && on_boundry')
-right = CompiledSubDomain('near(x[0],l) && on_boundry', l=xlength)
-top = CompiledSubDomain( 'pow(x[0]−X,2) + pow(x[1]−Y,2) < pow(120.0,2) && near(x[2],l) && on_boundary' ,X=xlength / 4 . , Y=ylength / 3 . , l=zlength)
-facets.set_all (0)
-top.mark(facets, 1)
-tr = Constant ((0.0,0.0,1000.0))
-null = Constant ((0.0,0.0,0.0))
-bc1=DirichletBC(V, null, left)
-bc2=DirichletBC(V, null, right)
-bs=[bc1, bc2]
-
-du= TrialFunction(V)
+from fenics import * 
+mesh = Mesh('')
+V = VectorFunctionSpace(mesh , 'P' , 1)
+cells = MeshFunction('size_t' , mesh , '')
+plot(cells , interactive = True)
+facets = MeshFunction('size_t' , mesh , '')
+plot(facets , interactive = True)
+dA = Measure('ds' , domain=mesh , subdomain_data = facets)
+dV = Measure('dx' , domain=mesh , subdomain_data = cells)
+left = CompiledSubDomain('near(x[0], 0) && on_boundary')
+right = CompiledSubDomain('near(x[0],l) && on_boundary' , l = #####)
+tr = Constant((0.0 , 0.0 , 1000.0))  #MPa
+null = Constant((0.0 , 0.0 , 0.0 ))
+bc = [DirchletBC)(V , null , facets , )]
+du = TrialFunction(V)
 del_u = TestFunction(V)
 u = Function(V)
-
-nu=0.375
-E=3000.0
-G= E/(2.0*(1.0+nu))
-
-lambada = 2.0*G*nu / (1.0-2.00*nu)
-mu = G
+nu = 0.3
+E = 3000 #in MPa
+G = E / (2.0 * (1.0 + nu))
+#Lame parameters (lambda has another meaning in python)
+lambada = 2.0*G*nu / (1.0 - 2.0*nu)
+mu = G 
 delta = Identity(3)
-#index notation
-i, j, k, l = indices(4)
-#deformation gradient
-F = as_tensor (u[i].dx(j) + delta[i,j] , (i,j))
+i , j , k , l = indices(4)
+F = as_tensor(u[i].dx(j) + delta[i,j] , (i,j))
 J = det(F)
-#right Cauchy-Green deformation tensor
-C = as_tensor(F[i,k]*F[i,j] , (k,j))
-#Grean-Lagrange strain tensor
-E = as_tensor(1./2.*( C[k,j] - delta[k,j] ) , (k,j))
-#second Piola-Kirchhoff stress tensor
-S = as_tensor(lambada*E[l,l]*delta[k,j] + 2.0*mu*E[k,j] , (k,j))
-#nominal stress
-P = as_tensor(F[i,j]*S[j,k] , (k,i))
-Form = P[k,i]*del_u[i].dx(k)*dV - tr[i]*del_u[i]*dA(l)
-Gain = derivative(Form, u, du)
-solve(Form==0, u, bc, J=Gain, \
-      solver_parameters={"newton_solver":{"linear_solver": "lu", "relative_tolerance": 1e-3} }, \
-      form_compiler_parameters={"cpp_optimize":True, "representation": "quadrature", "quadratue_degree" : 2} )
-
-\write out
-pwd='/calcul/CR02/'
-file = File(pwd+'nonlin_elastostatics_deformations.pvd')
-file << u
-\Cauchy stress tensor
-sigma = as_tensor(1./J*F[j,k]*P[k.i] , (j,i))
-sigma_dev = as_tensor(sigma[i,j] - 1.0/3.0*sigma[k,k]*delta[i,j] , (i,j))
-eqStress = as_tensor((3.0/2.0*sigma_dev[i,j]*sigma_dev[i,j])**0.5 , () )
-#calculating the equivalent stress by projecting
-eqS = project(eqStress, FunctionSpace(mesh, 'P', 1))
-file = File(pwd+'nonlin_elastostatics_eqStress.pvd')
-file << eqS
-
-
-
-
+C = as_tensor(F[i,k] *F[i,j] , (k,j))
+E = as_tensor(1./2.*(C[k,j] - delta[k,j]), (k,j))
+S = as_tensor(F[i,j] * S[j , k] , (k ,i))
+Form = P[k , i]*del_u[i].dx(k)*dV - tr[i]*del_u[i]*dA(1)
+Gain = derivative(Form , u , du)
+solve(Form == 0 , u , bc , J = Gain, \
+	solver_parameters={"newton_solver":{"linear_solver" : "lu"
+		, "relative_tolerance" : 1e-3} } ,
+	form_compiler_parameters = {"cpp_optimize" : True , "
+		representation" : "quadrature" , "quadrature_degree"
+		: 2}  )
 
